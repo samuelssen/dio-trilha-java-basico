ba26251d47980fc25459c405fdda8be0569a9b61
samuelsen <santossamuelsouza@gmail.com>
2025-02-23 23:07:21 -0300

    treinando sobre operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+
+    public static void main (String[] args){
+        int numero = 5;
+        boolean variavel = true; 
+        //repetição    
+        numero = numero + 2; 
+        //serve para incremento de +1
+        numero++;        
+        // ! para um booleano serve negar a variavel  
+        System.out.println(numero);
+        System.out.println(!variavel); 
+    }
+    
+}
